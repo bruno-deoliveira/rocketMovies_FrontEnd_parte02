8e4c88a99ab89173ae649ca1194d567cd86efa4e
Bruno <brunoalmeida0805@gmail.com>
2023-06-03 23:58:57 -0300

    page em desenvolvimento

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { api } from "../../services/api";
+import { useNavigate } from "react-router-dom";
+
 import { Container, Form } from "./style";
 
 import { Link } from "react-router-dom";
@@ -13,6 +17,73 @@ import { FiArrowLeft } from "react-icons/fi";
 import { Rating } from "./../../components/Rating/index";
 
 export function New() {
+  const [title, setTitle] = useState("");
+  const [rating, setRating] = useState("");
+  const [description, setDescription] = useState("");
+
+  const [links, setLinks] = useState([]);
+  const [newLink, setNewLink] = useState("");
+
+  const [tags, setTags] = useState([]);
+  const [newTag, setNewTag] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleAddLink() {
+    setLinks(prevState => [...prevState, newLink]);
+    setNewLink("");
+  }
+
+  function handleRemoveLink(deleted) {
+    setLinks(prevState => prevState.filter(link => link !== deleted));
+  }
+
+  function handleAddTag() {
+    setTags(prevState => [...prevState, newTag]);
+    setNewTag("");
+  }
+
+  function handleRemoveTag(deleted) {
+    setTags(prevState => prevState.filter(tag => tag !== deleted));
+  }
+
+  async function handleNewNote(){
+
+    if(!title){
+      return alert("Digite o título da nota")
+    }
+    
+    if (!rating) {
+      return alert("Informe sua nota de avaliação sobre o filme!");
+    }
+
+    if (newLink) {
+      return alert(
+        "Você deixou um link no campo para adicionar, mas não clicou em adicionar. Clique para adicionar ou deixe o campo vazio."
+      );
+    }
+
+    if (newTag) {
+      return alert(
+        "Você deixou uma tag no campo para adicionar, mas não clicou em adicionar. Clique para adicionar ou deixe o campo vazio."
+      );
+    }
+
+
+    await api.post("/notes", {
+      title,
+      rating,
+      description,
+      tags,
+      links,
+    });
+    alert("Notas criadas com sucesso");
+    navigate(-1);
+  }
+
+  
+
+
   return (
     <Container>
       <Header>
@@ -30,33 +101,67 @@ export function New() {
           </header>
 
           <div>
-            <Input placeholder="Titulo" />
+            <Input
+              placeholder="Titulo"
+              onChange={(e) => setTitle(e.target.value)}
+            />
 
             <Input //OBS RATING
               placeholder="Sua nota (de 0 a 5)"
               type="number"
               min="0"
               max="5"
+              value={rating}
+              onChange={(e) => setRating(e.target.value)}
             />
           </div>
 
-          <Textarea placeholder="Observações" />
+          <Textarea
+            placeholder="Observações"
+            onChange={(e) => setDescription(e.target.value)}
+          />
 
           <Section title="Links úteis">
-            <NoteItem value="https://rocketseat.com.br/" />
-            <NoteItem isNew placeholder="Novo link" />
+            {links.map((link, index) => (
+              <NoteItem
+                key={String(index)}
+                value={link}
+                onClick={() => handleRemoveLink(link)}
+              />
+            ))}
+
+            <NoteItem
+              isNew
+              placeholder="Novo link"
+              value={newLink}
+              onChange={(e) => setNewLink(e.target.value)}
+              onClick={handleAddLink}
+            />
           </Section>
 
           <Section title="Marcadores">
             <div className="tags">
-              <NoteItem value="React" />
-              <NoteItem isNew placeholder="Nova tag" />
+              {tags.map((tag, index) => (
+                <NoteItem
+                  key={String(index)}
+                  value={tag}
+                  onClick={() => handleRemoveTag(tag)}
+                />
+              ))}
+
+              <NoteItem
+                isNew
+                placeholder="Nova Tag"
+                onChange={(e) => setNewTag(e.target.value)}
+                value={newTag}
+                onClick={handleAddTag}
+              />
             </div>
           </Section>
 
           <div>
             <Button title="Excluir filme" />
-            <Button title="Salvar" />
+            <Button title="Salvar" onClick={handleNewNote} />
           </div>
         </Form>
       </main>
